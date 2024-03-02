af3a56c189b3203e0921edd67cdc606b708ce7de
Essam2004hekal <120648572+Essam2004hekal@users.noreply.github.com>
2024-03-02 21:02:31 +0200

    Add files via upload

---
+++ js-assignments/main.js
@@ -0,0 +1,61 @@
+// Assignment 1
+// let arr = [10, 15, 20, 25, 30];
+// let sum = 0;
+// for (let i = 0; i < arr. length; i++) {
+//     sum += arr[i];
+//     } 
+//     console. log(sum); 
+
+
+
+
+
+// Assignment 2 
+// const greeting = '   Hello world!   ';
+
+// console.log(greeting.trim());
+
+
+
+// Assignment 3
+// let num = prompt("enter number")
+// if (num %10 === 0) {
+//     console.log(true)
+// }else {
+//     console.log(false)
+// }
+
+
+// Assignment 4
+// let num1 = prompt("enter number")
+// let num2 = prompt("enter number")
+
+// let beverage = num1 > num2 ? num1 : num2;
+// console.log(beverage);
+
+
+
+
+
+// Assignment 5
+// String
+
+
+
+
+
+
+// Assignment 6
+// The number will be a string
+
+
+
+
+// Assignment 7
+// a logical operator that combines two or more conditions
+
+
+
+
+// Assignment 8
+// use the status (on or off) of the resulting indicators to determine whether a particular element is present in the arrays
